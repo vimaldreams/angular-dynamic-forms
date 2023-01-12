d2fef4c4fa58aba023a12c9d6f274caf4b75fa0e
vimaldreams <vimalkumar.ps10@gmail.com>
2023-01-12 10:49:29 +0530

    4 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,29 @@
-import { Component, VERSION } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'my-app',
   templateUrl: './app.component.html',
-  styleUrls: [ './app.component.css' ]
+  styleUrls: ['./app.component.css'],
 })
-export class AppComponent  {
-  name = 'Angular ' + VERSION.major;
+export class AppComponent implements OnInit {
+  dynamicForm: FormGroup;
+  submitted = false;
+
+  constructor(private formBuilder: FormBuilder) {}
+
+  ngOnInit() {
+    this.dynamicForm = this.formBuilder.group({});
+  }
+
+  get f() {
+    return this.dynamicForm.controls;
+  }
+  get t() {
+    return this.f.tickets as FormArray;
+  }
+
+  onChangeTextboxes(e) {}
+
+  onSubmit() {}
 }
